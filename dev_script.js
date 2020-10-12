091088e8c6e1fe0665ef0b24af97d3b904d5dd0c
niroshkumarh <niroshkumar2020@gmail.com>
2020-10-12 16:28:02 +0530

    important date UI added

--- dev_script.js
+++ dev_script.js
@@ -1,42 +1,37 @@
-const create_element = (type, obj = {}) => {
+const create_element = (type,obj) => {
     let elem = document.createElement(type)
-
+    console.log(obj)
     for (let [key, value] of Object.entries(obj)) {
-        elem[key] = value
+        console.log(key, value)
+        elem[`${key}`] = value
     }
-    return document.createElement(type)
+    return elem
 }
 
 const input_form = (i) => {
-    return `<div class="row">
-<center>Team Member ${i + 2}</center>
+    return `<div class="col-sm-12">
+<center class="text-center">Team Member ${i + 2}</center>
 <div class="col-sm-12">
-    <div class="col-sm-10 col-half-offset" id="${5*i+1}">
+    <div class="form-group">
         <div class="form-row">
-            <input type="text" name="name2" id="name2"
-                placeholder="Name / பெயர் " />
+            <input type="text" required placeholder="Name / பெயர் " name="name${i+1}" class="form-control"/>
         </div>
     </div>
-    <div class="col-sm-10 col-half-offset form-row my-2" id="${5*i+2}">
-        <select class="form-control"
-            name="Profession2" id="Profession2">
-            <option value="0">----- Profession -----</option>
+    <div class="form-group">
+        <select required name="profession${i+1}" class="form-control">
+            <option value="">----- Profession -----</option>
             <option value="Student">Student</option>
             <option value="Working">Working</option>
         </select>
     </div>
-    <div class="col-sm-10 col-half-offset form-row my-2" id="${5*i+3}">
-        <input type="text" name="CollegeWorkstationName2"
-            id="CollegeWorkstationName2"
-            placeholder="College/Workstation Name" />
+    <div class="form-group">
+        <input required name="collegeworkstationname${i+1}" type="text" placeholder="College/Workstation Name" />
     </div>
-    <div class="col-sm-10 col-half-offset form-row my-2" id="${5*i+4}">
-        <input type="email" name="email2" id="email2"
-            placeholder="Email/மின்னஞ்சல் முகவரி " />
+    <div class="form-group">
+        <input required name="email${i+1}" type="email" placeholder="Email/மின்னஞ்சல் முகவரி " />
     </div>
-    <div class="col-sm-10 col-half-offset form-row my-2" id="${5*i+5}">
-        <input type="text" name="phone2" id="phone2"
-            placeholder="Phone Number" />
+    <div class="form-group">
+        <input required name="phone${i+1}" type="text" placeholder="Phone Number" />
     </div>
 </div>
 </div>
@@ -49,45 +44,45 @@ const handle_team_change = (event) => {
     target_div.innerHTML = ""
     let no_of_objects = event.target.value
 
-    for (let i = 0; i < no_of_objects-1; i++) {
-        let div_elem = create_element('div', { 'id': `team_${i}`, 'className': 'team_div' })
-        // div_elem.style.height = "200px"
-        // div_elem.style['z-index'] = '100'
+    for (let i = 0; i < no_of_objects - 1; i++) {
+        let div_elem = create_element('div', { 'id': `team_${i}`, 'className': 'team_div col-sm-12' })
+        // // div_elem.style.height = "200px"
+        // // div_elem.style['z-index'] = '100'
         console.log(div_elem)
         div_elem.innerHTML = input_form(i)
-        target_div.append(div_elem)
+        target_div.innerHTML += input_form(i)
         console.log("here...")
     }
-} 
+}
 
 
 $("#login-form").submit(function (e) {
-	e.preventDefault()
+    e.preventDefault()
 
-	// document.getElementById("submit_btn").disabled = true
-	// document.getElementById("submit_btn").classList.add("disabled")
-	const Submit_btn = document.getElementById("submit_btn")
-	Submit_btn.value="Wait... "
-	Submit_btn.disabled=true
-	Submit_btn.classList.add("disabled̥")
-	// $("#alert_resp_box").show()
-	let form_data = new FormData(document.querySelector("#login-form"))
+    // document.getElementById("submit_btn").disabled = true
+    // document.getElementById("submit_btn").classList.add("disabled")
+    const Submit_btn = document.getElementById("submit_btn")
+    Submit_btn.value = "Wait... "
+    Submit_btn.disabled = true
+    Submit_btn.classList.add("disabled̥")
+    // $("#alert_resp_box").show()
+    let form_data = new FormData(document.querySelector("#login-form"))
 
-	fetch("https://script.google.com/macros/s/AKfycbw9mIxrlQ3_nCG_tPuLbVzpPAkp9mK238xr3dEGqXVG7XhhsUsm/exec",{
-		method:"POST",
-		body:form_data
-	}).then(response=>{
-		return response.json()
-	}).then(json_response=>{
-		if(json_response.result == "success"){
-			swal("Good Job !","Check your Email & join Whatsapp Group.","success").then(val=>{
-				window.location.reload()
-			})
-		}
-		else{
-			swal("Oops","Please try again after some time.","error")
-		}
-	})
+    fetch("https://script.google.com/macros/s/AKfycbw9mIxrlQ3_nCG_tPuLbVzpPAkp9mK238xr3dEGqXVG7XhhsUsm/exec", {
+        method: "POST",
+        body: form_data
+    }).then(response => {
+        return response.json()
+    }).then(json_response => {
+        if (json_response.result == "success") {
+            swal("Good Job !", "Check your Email & join Whatsapp Group.", "success").then(val => {
+                window.location.reload()
+            })
+        }
+        else {
+            swal("Oops", "Please try again after some time.", "error")
+        }
+    })
 
 
 })
